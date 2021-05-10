271952087c5512cbf960ff841f20e1f54e5fa4e0
mattes-orga01-dev01 <mattes-orga01-dev01@wohland.de>
2021-05-10 16:33:26 +0200

    Initialer Lokaler Commit

---
+++ webapp/Component.js
@@ -0,0 +1,9 @@
+jQuery.sap.declare("ZMW_FACETS01.Component");
+sap.ui.getCore().loadLibrary("sap.ui.generic.app");
+jQuery.sap.require("sap.ui.generic.app.AppComponent");
+
+sap.ui.generic.app.AppComponent.extend("ZMW_FACETS01.Component", {
+	metadata: {
+		"manifest": "json"
+	}
+});
